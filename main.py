c8a9cac516dd3b880a667dd001fd2d3e75cac6f8
Phan Anh <phananh1096@gmail.com>
2020-05-25 18:00:58 -0700

    Fixed CSV Export issue

--- main.py
+++ main.py
@@ -14,61 +14,64 @@ def extractJobElement(soupObject, elementToExtract):
 
 if __name__ == "__main__":
 
-    # for page in range(0, 200, 10):
-    # Required driver for selenium
-    driver = webdriver.Chrome("./chromedriver")
-    driver.implicitly_wait(4)
+    for page in range(0, 200, 10):
 
-    # List of Attributes to be extracted   
-    attributeList = ["jobtitle" , "location", "company", "salary", "sponsoredGray"]
-    
-    df = pd.DataFrame(columns=["Title", "Location", "Company", "Salary", "Sponsored", "Description"])
-    # driver.get("https://www.indeed.com/jobs?q=Software+Engineering+Intern&l=San+Francisco%2C+CA&radius=50&start="+str(page))
-    driver.get("https://www.indeed.com/jobs?q=Software+Engineering+Intern&l=San+Francisco%2C+CA&radius=50")
+        # Set up Attribute list and DataFrame
+        attributeList = ["jobtitle" , "location", "company", "salary", "sponsoredGray"]
+        df = pd.DataFrame(columns=["Title", "Location", "Company", "Salary", "Sponsored", "Description"])
+        
+        # Set up Selenium driver
+        driver = webdriver.Chrome("./chromedriver")
+        driver.implicitly_wait(4)
+        driver.get("https://www.indeed.com/jobs?q=Software+Engineering+Intern&l=San+Francisco%2C+CA&radius=50&start="+str(page))
 
-    all_jobs = driver.find_elements_by_class_name("result")
+        # Get all jobs in current page
+        all_jobs = driver.find_elements_by_class_name("result")
 
-    count = 0
-    for posting in all_jobs:
-        try:
-            html = posting.get_attribute('innerHTML')
-            soup = BeautifulSoup(html, 'html.parser')
-
-            
-            # print(soup.prettify())       
-            jobEntry = {
-                "Title": extractJobElement(soup, attributeList[0]), 
-                "Location": extractJobElement(soup, attributeList[1]), 
-                "Company": extractJobElement(soup, attributeList[2]), 
-                "Salary": extractJobElement(soup, attributeList[3]), 
-                "Sponsored":extractJobElement(soup, attributeList[4])
-                }
-            
-            summary = posting.find_elements_by_class_name("summary")[0]
+        # Loop through postings on current page to extract information
+        for posting in all_jobs:
             try:
-                summary.click()
-            except:
+                html = posting.get_attribute('innerHTML')
+                soup = BeautifulSoup(html, 'html.parser')
+
+                jobEntry = {
+                    "Title": extractJobElement(soup, attributeList[0]), 
+                    "Location": extractJobElement(soup, attributeList[1]), 
+                    "Company": extractJobElement(soup, attributeList[2]), 
+                    "Salary": extractJobElement(soup, attributeList[3]), 
+                    "Sponsored":extractJobElement(soup, attributeList[4])
+                    }
+                
+                summary = posting.find_elements_by_class_name("summary")[0]
+                
+                # Error handling for pop-up
                 try:
-                    closeButton = driver.find_elements_by_class_name("popover-x-button-close")
-                    closeButton.close()
                     summary.click()
                 except:
-                    pass
-            
-            descriptionTestKeywords = ["vjs-desc", "jobDescriptionText"]
-            description = "None"
-            for keyword in descriptionTestKeywords:
-                try:
-                    description = driver.find_element_by_id(keyword).text.replace("\n","")
-                    break
-                except:
-                    pass
+                    try:
+                        closeButton = driver.find_elements_by_class_name("popover-x-button-close")
+                        closeButton.close()
+                        summary.click()
+                    except:
+                        pass
+                
+                # Tries different id keywords for job description
+                descriptionTestKeywords = ["vjs-desc", "jobDescriptionText"]
+                description = "None"
+                for keyword in descriptionTestKeywords:
+                    try:
+                        description = driver.find_element_by_id(keyword).text.replace("\n","")
+                        break
+                    except:
+                        pass
 
-            jobEntry['Description'] = description
-            df = pd.concat(df, pd.DataFrame(jobEntry, ignore_index=True))
-            print(jobEntry)
-        except:
-            print("Error with this listing!")
-            pass
+                jobEntry['Description'] = description
 
-        df.to_csv("./data/test2.csv", index=False)
+                # Appends current post to DataFrame
+                df = df.append(jobEntry, ignore_index=True)
+            except:
+                print("Error with this listing!")
+                pass        
+
+    # Exports to CSV
+    df.to_csv("./data/test4.csv", index=False)
